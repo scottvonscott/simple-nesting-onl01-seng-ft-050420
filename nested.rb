1bd125a371c2829dcdd3bf1472d617636eac1121
Scott  Keplinger <skeplin17@gmail.com>
2020-05-10 19:41:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-programmer_hash["grace_hopper"][:known_for, :languages]
-
+		 
+programmer_hash[:grace_hopper][:known_for, :languages]
 end
 
 def alan_kay_is_known_for
